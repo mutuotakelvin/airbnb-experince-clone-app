22235f956cc2fb0da5551d649e99d661eb77fd39
mutuotakelvin <kelvinmutuota749@gmail.com>
2022-01-27 02:20:34 +0300

    Add Hero and Card component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Navbar from './components/navbar/Navbar';
+import Hero from './components/hero/Hero';
+import Card from './components/card/Card';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Hero/>
+      <Card/>
       <h1>App component</h1>
     </div>
   );
