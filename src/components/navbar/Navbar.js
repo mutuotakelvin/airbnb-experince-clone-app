931a5139f01056f4efaea9921910bdba960a1b68
mutuotakelvin <kelvinmutuota749@gmail.com>
2022-01-27 01:26:04 +0300

    Define navBar component

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import navImg from '../../images/logo.png'
 
 class Navbar extends Component {
     render() {
         return (
-            <div>
-                <h1>Nav bar</h1>
-            </div>
+            <nav>
+                <img src={navImg} alt='airbnb logo'/>
+            </nav>
         );
     }
 }
